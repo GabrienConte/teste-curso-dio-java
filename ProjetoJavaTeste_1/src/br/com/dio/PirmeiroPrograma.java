4162432642ca429725a66a6dec6ecb889ac75394
GabrienConte <gabrielconte0@gmail.com>
2022-05-15 17:51:58 -0300

    Mudança de diretorio

---
+++ ProjetoJavaTeste_1/src/br/com/dio/PirmeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PirmeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		int a = 5;
+		int b = 5;
+		System.out.println("Olá Mundo!" + a + b + gato.toString());
+	}
+
+} 
